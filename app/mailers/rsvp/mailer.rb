21d87c9dc8bbd96521224a973d3e1bba5b81efce
Kartikey Chauhan <me@cskartikey.dev>
2026-04-20 03:05:29 +0530

    fix(rsvp): update RSVP confirmation email to use new confirmation code variable

--- app/mailers/rsvp/mailer.rb
+++ app/mailers/rsvp/mailer.rb
@@ -1,7 +1,7 @@
 class Rsvp::Mailer < ApplicationMailer
   def signup_confirmation(rsvp)
     @email = rsvp.email
-    @confirmation_url = confirm_rsvp_url(token: rsvp.confirmation_token)
+    @confirm_code = rsvp.confirmation_token
     # The "to" address is required by Action Mailer but will be overwritten
     # by the email provided in the view. A subject is also not required here
     # as Loops will use the subject from the editor.
